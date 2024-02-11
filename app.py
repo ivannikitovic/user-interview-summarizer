4d0649ab7fb335f33d0c121c74206b0b5d2ea740
Ivan Nikitovic <ivan.bnikitovic@gmail.com>
2024-02-10 19:29:33 -0500

    add logic

---
+++ app.py
@@ -0,0 +1,59 @@
+import os
+import streamlit as st
+from media_converter import convert_to_wav
+from transcript_generator import extract_transcript
+
+st.title('User Interview Transcription and Summarizer')
+
+# User input
+with st.form(key='form'):
+    # Text input for the OpenAI API token
+    openai_token = st.text_input('Enter your OpenAI API token:')
+
+    # Text area for the interview description prompt
+    interview_prompt = st.text_area("Provide a brief description \
+                                    of the interview:")
+
+    # File uploader for video/audio file
+    file_upload = st.file_uploader("Upload interview file (audio or video):",
+                                   type=['mp3', 'mp4', 'm4a', 'avi', 'wav'])
+
+    # Submit button for the form
+    submit_button = st.form_submit_button(label='Submit')
+
+# Actions to take upon form submission
+if submit_button:
+    if openai_token and file_upload and interview_prompt:
+        # Display the input for confirmation (For demonstration purposes)
+        # You would replace this section with your processing logic
+        st.success("Form submitted successfully.")
+
+        with st.status("Generating ... This could take a few minutes."):
+            st.write("Converting media ...")
+            wav_output_path = convert_to_wav(file_upload)
+            print("Media converted successfully.")
+            print("wav output path: ", wav_output_path)
+
+            st.write("Generating transcript ...")
+            transcript_output_path = extract_transcript(wav_output_path)
+            print("Transcript generated successfully.")
+            print("ts output path: ", transcript_output_path)
+
+            os.remove(wav_output_path)
+
+            # Read the transcript file into a string
+            with open(transcript_output_path, "r") as file:
+                transcript = file.read()
+
+            os.remove(transcript_output_path)
+
+        # Create a download button for the transcript
+        st.download_button(label="Download Transcript",
+                        data=transcript,
+                        file_name="transcript.txt",
+                        mime="text/plain")
+        
+        print("done")
+
+    else:
+        st.error("Please fill in all the fields and upload a file.")
---
+++ transcript_generator.py
@@ -0,0 +1,16 @@
+import whisper
+
+def extract_transcript(input_path, output_path=None, model_size="base"):
+    if not output_path:
+        output_path = output_path = f"./temp_files/transcript.txt"
+
+    speech_to_text(input_path, output_path, model_size)
+
+    return output_path
+
+def speech_to_text(input_path, output_path, model_size):
+    model = whisper.load_model(model_size)
+    result = model.transcribe(input_path)
+
+    with open(output_path, "w") as text_file:
+        text_file.write(result["text"])
