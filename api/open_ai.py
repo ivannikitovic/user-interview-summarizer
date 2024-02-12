8253510f5fb4ecc820cef79682e10414a538011d
Ivan Nikitovic <ivan.bnikitovic@gmail.com>
2024-02-11 23:15:08 -0500

    working on OpenAI api

---
+++ api/open_ai.py
@@ -0,0 +1,81 @@
+from openai import OpenAI
+
+ERR_MSG = "ERRORERRORERRORERROR"
+DONE_MSG = "LABELIZATIONCOMPLETE"
+
+
+def send_message(client, thread, assistant, payload):
+    _ = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=payload
+    )
+
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+    )
+
+    # print(run.status)
+
+    while run.status != "completed":
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id
+        )
+
+    messages = client.beta.threads.messages.list(
+        thread_id=thread.id
+    )
+
+    response = None
+    for content in messages.data[0].content:
+        if content.type == 'text':
+            response = content.text.value
+
+    return response
+
+def labelize_transcript(api_key, transcript):
+    client = OpenAI(
+        api_key=api_key,
+    )
+
+    assistant = client.beta.assistants.create(
+        instructions=f"You will be provided a user interview transcript. \
+            Assign speaker labels to the transcript \
+            (Interviewer, Interviewee). \
+            Insert a new line between speaker blocks. \
+            Return just the labeled transcript, \
+            without any additional text before or after. \
+            Append {DONE_MSG} at the end, alone on a new line. \
+            Please fix any semantic and logical errors \
+            you find within the script since it's extracted using Whisper. \
+            If any of this is not possible or you encounter an error, \
+            just write {ERR_MSG}",
+        # model="gpt-4-turbo-preview",
+        model="gpt-3.5-turbo-0125",
+    )
+
+    thread = client.beta.threads.create()
+
+    response = send_message(client, thread, assistant, transcript)
+    labelized = response
+
+    while labelized[-20:] not in [ERR_MSG, DONE_MSG]:
+        # print(labelized[-20:])
+        if labelized[-20:] == ERR_MSG:
+            return ERR_MSG
+
+        response = send_message(client, thread, assistant, "Continue")
+        labelized += response
+
+    return labelized
+
+
+if __name__ == "__main__":
+    with open("./test.txt", 'r') as file:
+        transcript = file.read()
+    result = labelize_transcript("OPEN_AI_TOKEN",
+                                 transcript)
+
+    print(result)
